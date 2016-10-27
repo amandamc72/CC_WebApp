0741ea553b43cbb0b6c828f4128b7019905de213
amandamc72 <amccart8@emich.edu>
2016-10-27 11:33:17 -0400

    test reg form submit

--- scripts/controller.js
+++ scripts/controller.js
@@ -57,19 +57,17 @@ function loginFormToJSON() {
 }
 
 // Register form listener
-$(function() {
-	$('#registerForm').submit(function(e) {
-		e.preventDefault();
-		console.log('form submitted');
-		var email = document.getElementById("registerEmail").value;
-		var validEmail = emailCheck(email);
-		if (validEmail){
-			register();
-		} else {
-			$('#registerEmail').effect( "shake", {times:4}, 1000 );
-			$("#error").html("Invalid email address");
-		}
-	});
+$('#registerForm').submit(function(e) {
+	e.preventDefault();
+	console.log('form submitted');
+	var email = document.getElementById("registerEmail").value;
+	var validEmail = emailCheck(email);
+	if (validEmail){
+		register();
+	} else {
+		$('#registerEmail').effect( "shake", {times:4}, 1000 );
+		$("#error").html("Invalid email address");
+	}
 });
 
 // Register callback to API
